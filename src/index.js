36d01c202483c7275836b46f4cc7f9e1f2974c8f
KorvinM <km@korvin.org>
2019-08-13 11:49:12 +0100

    add env variables support;add .gitgnore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+/* index.js
+ */
+
+// import dotenv config before any local file imports
+import 'dotenv/config';
 console.log('Hello modernjs on the backend.');
+console.log(process.env.MY_SECRETTHING);
