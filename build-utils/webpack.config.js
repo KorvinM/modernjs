a1de755947c649229a1ef50b17355dcb8d2c5df0
KorvinM <km@korvin.org>
2019-08-14 10:29:09 +0100

    add analyzer and visualiser reporting as an optional build script

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -2,8 +2,19 @@ const webpackMerge = require('webpack-merge');
 
 const commonConfig = require('./webpack.common.js');
 
-module.exports = ({ env }) => {
+//set optional addons
+const getAddons = addonsArgs => {
+  const addons = Array.isArray(addonsArgs)
+    ? addonsArgs
+    : [addonsArgs];
+
+  return addons
+    .filter(Boolean)
+    .map(name => require(`./addons/webpack.${name}.js`));
+};
+
+module.exports = ({ env, addon }) => {
   const envConfig = require(`./webpack.${env}.js`);
 
-  return webpackMerge(commonConfig, envConfig);
+  return webpackMerge(commonConfig, envConfig, ...getAddons(addon));
 };
