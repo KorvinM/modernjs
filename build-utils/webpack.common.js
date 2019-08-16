72c60e420d5fab5d5b29ea9ff80272ccded1070a
KorvinM <km@korvin.org>
2019-08-16 14:11:33 +0100

    tidy

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,6 +1,6 @@
-const path = require('path'); 
+const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   //use src/index.js as entry point to bundle. If the src/index.js file imports other JavaScript files, bundle them as well
   entry: './src/index.js',
