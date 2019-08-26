8bfeacbcd5a774919ce36c5012a8643c93a20a69
KorvinM <km@korvin.org>
2019-08-26 01:58:17 +0100

    optimize css in production

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,6 +1,7 @@
 const Dotenv = require('dotenv-webpack'),
       CssExtract = require('mini-css-extract-plugin'),
-      UglifyJs = require('uglifyjs-webpack-plugin');
+      UglifyJs = require('uglifyjs-webpack-plugin'),
+      OptimizeCssAssets = require('optimize-css-assets-webpack-plugin');
 module.exports ={
   mode: 'production',
   devtool: 'source-map',
@@ -17,6 +18,14 @@ module.exports ={
       path: './.env-prod',
     }),
     new CssExtract(),
+    new OptimizeCssAssets({
+      //assetNameRegExp: /\.optimize\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default', { discardComments: { removeAll: true } }],
+      },
+      canPrint: true
+    })
   ],
   optimization: {
     minimizer: [new UglifyJs()],
